674d08e68d1ff73e4f5be7644dcf4483da6ffd0a
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-03-12 02:34:38 +0500

    Use string, number, and boolean type

---
+++ src/components/Greet.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      {props.isLoggedIn
+        ? `Hi ${props.name}, you've ${props.messageCount} unread messages!`
+        : `Hi Guest!`}
+    </div>
+  );
+};
+
+export default Greet;
