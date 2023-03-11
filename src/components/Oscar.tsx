0e61f3d86d14fce3c6fc2da7af3a6555465bcb56
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-03-12 02:47:08 +0500

    Use React Component as children type

---
+++ src/components/Oscar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+type OscarProps = {
+  children: React.ReactNode;
+};
+const Oscar = (props: OscarProps) => {
+  return <div>{props.children}</div>;
+};
+
+export default Oscar;
